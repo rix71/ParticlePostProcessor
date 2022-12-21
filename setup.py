147c3b582abba152778b7faf084991f02a6a55e4
Enriko <enriko.siht11@gmail.com>
2022-12-21 18:29:58 +0200

    Remove author

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 setup(name="ParticlePostProcessor",
       version="0.1",
       description="Post-processing for particle tracking model",
-      author="Enriko Siht",
       packages=find_packages(),
       entry_points={
           "console_scripts": ["ppp=src.main:main"]
