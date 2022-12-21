e685b13be5774c88b3f05c35a51b567ad34fac71
Enriko <enriko.siht11@gmail.com>
2022-12-21 19:31:36 +0200

    Added process success/fail

--- tests/cli_test.sh
+++ tests/cli_test.sh
@@ -3,19 +3,32 @@ function print_separator {
     echo "------------------------------------------------------------"
 }
 
+Red='\033[0;31m'
+Green='\033[0;32m'
+NC='\033[0m'
+function check_process_success {
+    err=$?
+    if [ $err -eq 0 ]; then
+        echo -e "${Green}Process success${NC}"
+    else
+        echo -e "${Red}Process failed:${NC} $err"
+    fi
+    print_separator
+}
+# ------------------------------------------------------------------
 # Basic help test
 ppp -h
-print_separator
-ppp --help
-print_separator
+check_process_success
+# Should fail (wrong command)
+ppp -Q
+check_process_success
 # Process command test
 ppp process -h
-print_separator
-ppp process --help
-print_separator
+check_process_success
 ppp process -s ./data/particles.nc --topo ./data/topo.nc -o ./output.nc
-print_separator
+check_process_success
 # Should fail (file exists)
 ppp process -s ./data/particles.nc --topo ./data/topo.nc -o ./output.nc
-print_separator
+check_process_success
 ppp process -O -s ./data/particles.nc --topo ./data/topo.nc -o ./output.nc
+check_process_success
